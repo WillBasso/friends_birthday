681f5cbcdd662f286e3b35d8627277815896b7cd
WillBasso <willianalexbasso.wb@gmail.com>
2026-01-14 13:43:09 -0300

    Change base

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/friends_birthday">
       <App />
     </BrowserRouter>
   </StrictMode>
