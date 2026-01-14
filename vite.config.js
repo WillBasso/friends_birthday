cca0d68338903c7f22586eb4c5e00429d53001c7
WillBasso <willianalexbasso.wb@gmail.com>
2026-01-14 13:26:06 -0300

    change base

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "https://github.com/WillBasso/friends_birthday",
+  base: "https://willbasso.github.io/friends_birthday/",
 });
